1ebc702790352704ddcc68da40747bd6f9d520e7
Tunlaya-Sanphokha <orn.tunlaya3643@gmail.com>
2023-09-25 14:37:29 +0000

    add root to movies#index

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rottenpotatoes::Application.routes.draw do
     }
   resources :movies
   # map '/' to be a redirect to '/movies'
-  root :to => redirect('/movies')
+  # root :to => redirect('/movies')
+  root to: 'movies#index'
   post '/movies/search_tmdb' => 'movies#search_tmdb', :as => 'search_tmdb'
 end
